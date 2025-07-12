e10cc70ad7f1b6f6c5dfd345efc248014602e2bb
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-12 10:49:30 +0300

    [hw-2] Отобразила рестораны из моковых данных

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,29 @@
 import {createRoot} from 'react-dom/client';
+import {restaurants} from '../materials/mock.ts';
+
 
 const root = document.getElementById('root') as HTMLElement;
-
 const reactRoot = createRoot(root);
 
-console.log(reactRoot);
+reactRoot.render(
+    <div className='restaurants'>
+        <h1>Рестораны</h1>
+        {restaurants.map((restaurant) => (
+            <div className="restaurant">
+                <h2 className='restaurant'>{restaurant.name}</h2>
+                <h3>Меню</h3>
+                <ul>
+                    {restaurant.menu.map((item) => (
+                        <li>{item.name}</li>
+                    ))}
+                </ul>
+                <h3>Отзывы</h3>
+                <ul>
+                    {restaurant.reviews.map((review) => (
+                        <li>{review.text}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+    </div>
+);
