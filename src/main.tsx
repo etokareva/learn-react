7f727dae264227033d436fd5ce647ab9f96b7728
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-13 12:44:18 +0300

    [hw-3] Убрала зависимости из main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import {createRoot} from 'react-dom/client';
-import {restaurants} from '../materials/mock.ts';
-import {App} from './components/app/App.tsx';
+import {App} from './components/app/app.tsx';
 
 
 const root = document.getElementById('root') as HTMLElement;
 const reactRoot = createRoot(root);
 
-reactRoot.render(<App restaurants={restaurants} title='Рестораны' />);
+reactRoot.render(<App/>);
