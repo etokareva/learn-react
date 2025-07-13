5f879e7eaae3651e4d4daf079bb600a890a89777
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-13 17:28:17 +0300

    [hw-4] Scroll progress и конфиг для импорта css

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,8 +1,10 @@
 import type {PropsWithChildren} from 'react';
+import {ScrollProgress} from '../scroll-progress/scroll-progress.tsx';
 
 export const Layout = ({children} : PropsWithChildren) => {
     return (
         <>
+            <ScrollProgress />
             <header></header>
             <main>{children}</main>
             <aside></aside>
