6dc680e48b26bbf80ab29d8ea229e0afcc5f1d1f
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-13 14:24:05 +0300

    [hw-4] Добавила форму отзыва

--- src/components/restaurant/restaurant.tsx
+++ src/components/restaurant/restaurant.tsx
@@ -1,6 +1,7 @@
 import type {RestaurantModel} from '../../models/restaurant.model.ts';
 import {Counter} from '../counter/counter.tsx';
 import {Menu} from '../menu/menu.tsx';
+import {ReviewForm} from '../review-form/review-form.tsx';
 import {Reviews} from '../reviews/reviews.tsx';
 
 export const Restaurant = ({restaurant}: {restaurant: RestaurantModel}) => {
@@ -11,6 +12,7 @@ export const Restaurant = ({restaurant}: {restaurant: RestaurantModel}) => {
             <h2>{name}</h2>
             <Menu menu={menu}/>
             {reviews?.length ? <Reviews reviews={reviews}/> : <div>Нет отзывов</div>}
+            <ReviewForm />
             <Counter/>
         </div>
     )
