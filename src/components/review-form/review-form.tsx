2e8d54a14c7e45589f613b3ac3b397c5e7b64b5e
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-19 20:25:48 +0300

    [hw-4] Кастомный хук для редьюсера формы

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -1,48 +1,52 @@
-import {useReducer} from 'react';
-import {CLEAR_FORM_ACTION, SET_NAME_ACTION, SET_RATING_ACTION, SET_TEXT_ACTION} from './review-form.actions.ts';
-import {reviewFormReducer} from './review-form.reducer.ts';
-import {INITIAL_STATE} from './review-form.state.ts';
-
+import {useReviewForm} from './review-form.hook';
 
 export const ReviewForm = () => {
-    const [form, dispatch] = useReducer(reviewFormReducer, INITIAL_STATE);
-    const {name, text, rating} = form;
+    const {
+        formState: {name, text, rating},
+        setName,
+        setText,
+        setRating,
+        clearForm
+    } = useReviewForm();
 
     return (
         <form>
             <div className="form-group">
                 <label>Имя</label>
-                <input type="text"
-                       value={name}
-                       onChange={(event) => {
-                           dispatch({type: SET_NAME_ACTION, payload: event.target.value});
-                       }}/>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
             </div>
             <div className="form-group">
                 <label>Текст</label>
-                <textarea value={text}
-                          onChange={(event) => {
-                              dispatch({type: SET_TEXT_ACTION, payload: event.target.value});
-                          }}/>
+                <textarea
+                    value={text}
+                    onChange={(event) => setText(event.target.value)}
+                />
             </div>
             <div className="form-group">
                 <label>Оценка (1 - 5)</label>
-                <input type="number"
-                       min="1"
-                       max="5"
-                       value={rating}
-                       onChange={(event) => {
-                           const value = parseInt(event.target.value);
-                           if (value >= 1 && value <= 5) {
-                               dispatch({type: SET_RATING_ACTION, payload: value});
-                           }
-                       }}
+                <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    value={rating}
+                    onChange={(event) => {
+                        const value = parseInt(event.target.value);
+                        if (value >= 1 && value <= 5) {
+                            setRating(value);
+                        }
+                    }}
                 />
             </div>
-            <button onClick={() => {
-                dispatch({type: CLEAR_FORM_ACTION});
-            }}>Очистить
+            <button
+                type="button"
+                onClick={clearForm}
+            >
+                Очистить
             </button>
         </form>
-    )
+    );
 };
