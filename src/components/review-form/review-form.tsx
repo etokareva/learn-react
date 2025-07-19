05fafb6973a48d12744cde0e86592d221026af46
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-19 20:29:29 +0300

    [hw-4] Добавила проп, чтобы страница не перезагружалась при нажатии кнопки

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -10,7 +10,7 @@ export const ReviewForm = () => {
     } = useReviewForm();
 
     return (
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
             <div className="form-group">
                 <label>Имя</label>
                 <input
