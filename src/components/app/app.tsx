802c4cbcc48b24b3aefa49debf64dcfffbf570e3
Ekaterina Tokareva <etokareva1990@gmail.com>
2025-07-22 13:39:38 +0300

    [hw-5] Добавила reset css

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import {restaurants} from '../../../materials/mock.ts';
 import {Restaurants} from '../../pages/restaurants/restaurants.tsx';
 import {Layout} from '../layout/layout.tsx';
+import './reset.css';
 
 export const App = () => {
     return (
